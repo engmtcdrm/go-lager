76103605c6cc379e23ed40156fac7d93f3935d58
John <deedsoftheforsaken@gmail.com>
2025-10-28 17:14:19 -0400

    Cleaned up commented out code. Added a bit more info on logging and expected output locations.

--- examples/main.go
+++ examples/main.go
@@ -1,57 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log/slog"
 
 	"github.com/engmtcdrm/go-lager"
 )
 
 func main() {
-	// debug := flag.Bool("debug", false, "enable debug logging")
-	// flag.Parse()
+	debug := flag.Bool("d", false, "enable debug logging")
+	trace := flag.Bool("t", false, "enable trace logging")
+	flag.Parse()
 
-	// logFile := lager.Init("___app.log", *debug)
-	// defer logFile.Close()
+	level := lager.LevelInfo
 
-	// indent := 4
+	if *debug {
+		level = lager.LevelDebug
+	} else if *trace {
+		level = lager.LevelTrace
+	}
 
-	// m := NewManager("___app.log")
-
-	// m.Info("does this work")
-
-	f, err := lager.Init("___app.log", false)
+	f, err := lager.Init("___app.log", level)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	lager.Info("testing slog info")
-	lager.Error("testing slog error")
-	lager.Debug("testing slog debug")
-	lager.Warn("testing slog warn")
-	lager.Trace("testing slog trace")
-
-	// lager.Trace("Application started")
-	// lager.Debug("This is a debug message")
-	// lager.DebugIndent("This is an indented debug message", indent)
-	// lager.Debug("")
-	// slog.With("indent", indent)
-	// ph := lager.NewPlainHandler(os.Stdout)
-	// slog.NewTextHandler(os.Stderr, nil)
-	// logger := slog.New(ph)
-	// logger.WithGroup("main")
-	// logger.Info("This is a key check", slog.Int("indent", indent))
-	slog.Info("This is a key check")
-	// lager.Info("We made it!")
-	// lager.InfoIndent("This is indented info", indent)
-	// lager.Info("")
-	// lager.Warn("This is a warning")
-	// lager.WarnIndent("This is an indented warning", indent)
-	// lager.Warn("")
-	// lager.Error("This has failed badly")
-	// lager.ErrorIndent("This is an indented error", indent)
-	// lager.Error("")
-
-	// fmt.Print("hellothere!")
-	// fmt.Println("only in stdout")
+	slog.Info("(FILE|STDOUT): testing slog info")
+	slog.Error("(FILE|STDERR): testing slog error")
+	slog.Debug("(FILE|STDERR): testing slog debug")
+	slog.Warn("(FILE|STDERR): testing slog warn")
+	lager.Trace("(FILE|STDERR): testing slog trace")
+	fmt.Println("(STDOUT): hello there!")
 }
