1c059f4f6ab033fa70a27e5f295fb1adfa8d01e0
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-11-06 21:31:33 -0500

    Renamed StreamHandler to streamHandler to internalize it. Added StreamType to specify either STDOUT or STDERR for the writer Refactored Enabled around minimum logging Starting improving Handle function to be more dynamic base on needs

--- stream_handler.go
+++ stream_handler.go
@@ -2,38 +2,60 @@ package lager
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log/slog"
+	"os"
+	"runtime"
 	"sync"
+	"time"
+
+	"github.com/engmtcdrm/go-ansi"
 )
 
-// StreamHandler prints only the message text
-type StreamHandler struct {
+type StreamType int
+
+const (
+	StreamStdout StreamType = iota
+	StreamStderr
+)
+
+// streamHandler prints only the message text
+type streamHandler struct {
 	w    io.Writer
 	mu   *sync.Mutex
-	opts Options
+	opts HandlerOptions
 }
 
-func NewStreamHandler(w io.Writer, opts *Options) *StreamHandler {
-	h := &StreamHandler{
-		w:  w,
-		mu: &sync.Mutex{},
+// newStreamHandler creates a new StreamHandler with the given writer and options
+func newStreamHandler(st StreamType, opts *HandlerOptions) *streamHandler {
+	if opts == nil {
+		opts = &HandlerOptions{}
 	}
 
-	if opts != nil {
-		h.opts = *opts
+	var w io.Writer
+	if st == StreamStdout {
+		w = os.Stdout
+	} else {
+		w = os.Stderr
 	}
 
-	if h.opts.Level == nil {
-		h.opts.Level = LevelInfo
+	return &streamHandler{
+		w:    w,
+		mu:   &sync.Mutex{},
+		opts: *opts,
 	}
-	return h
 }
 
 // Enabled checks if the handler is enabled for the given log level
-func (h *StreamHandler) Enabled(ctx context.Context, level slog.Level) bool {
+func (h *streamHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	minLevel := LevelInfo
+	if h.opts.Level != nil {
+		minLevel = h.opts.Level.Level()
+	}
+
 	if h.opts.Enablers == nil {
-		return level >= h.opts.Level.Level()
+		return level >= minLevel
 	}
 
 	for _, enable := range h.opts.Enablers {
@@ -46,10 +68,32 @@ func (h *StreamHandler) Enabled(ctx context.Context, level slog.Level) bool {
 }
 
 // Handle processes a log record and writes the message to the handler's output
-func (h *StreamHandler) Handle(ctx context.Context, r slog.Record) error {
+func (h *streamHandler) Handle(ctx context.Context, r slog.Record) error {
 	// Create a buffer to hold the final output
 	buf := make([]byte, 0, 512)
-	buf = append(buf, r.Message...)
+
+	if h.opts.AddTime && !r.Time.IsZero() {
+		buf = append(buf, r.Time.Round(0).Format(time.RFC3339)...)
+		buf = append(buf, " - "...)
+	}
+
+	if h.opts.AddLevel {
+		buf = append(buf, levelString(r.Level)...)
+		buf = append(buf, ": "...)
+	}
+
+	if h.opts.AddSource && r.PC != 0 {
+		fs := runtime.CallersFrames([]uintptr{r.PC})
+		f, _ := fs.Next()
+		buf = append(buf, slog.String(slog.SourceKey, fmt.Sprintf("%s:%d", f.File, f.Line)).Value.String()...)
+		buf = append(buf, " - "...)
+	}
+
+	buf = append(buf, r.Message+"\n"...)
+
+	if h.opts.NoColor {
+		buf = []byte(ansi.StripCodes(string(buf)))
+	}
 
 	// Write to stream
 	h.mu.Lock()
@@ -59,7 +103,11 @@ func (h *StreamHandler) Handle(ctx context.Context, r slog.Record) error {
 }
 
 // WithAttrs returns a new handler with the given attributes added
-func (h *StreamHandler) WithAttrs([]slog.Attr) slog.Handler { return h }
+func (h *streamHandler) WithAttrs([]slog.Attr) slog.Handler {
+	return h
+}
 
 // WithGroup returns a new handler with the given group name
-func (h *StreamHandler) WithGroup(string) slog.Handler { return h }
+func (h *streamHandler) WithGroup(string) slog.Handler {
+	return h
+}
