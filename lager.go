8a7ef059481bea9d17f81281dca312bfcc1feed0
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-12-24 17:42:06 -0500

    Refactored Init func into a Lager struct to simplify customization

--- lager.go
+++ lager.go
@@ -1,33 +1,109 @@
+// Package lager provides a configurable logging setup using Go's slog package.
+// It supports logging to files, standard output, and standard error with
+// customizable options for log levels, formatting, and more.
 package lager
 
 import (
-	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"strings"
 )
 
-func Init(logFileNm string, level slog.Leveler) (*os.File, error) {
-	f, err := os.OpenFile(logFileNm, os.O_APPEND|os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return nil, err
+type Lager struct {
+	noFile     bool
+	noStdout   bool
+	noStderr   bool
+	logFileNm  string
+	optsFile   *HandlerOptions
+	optsStdout *HandlerOptions
+	optsStderr *HandlerOptions
+}
+
+func NewLager(level slog.Leveler, logFileNm string) *Lager {
+	l := &Lager{
+		logFileNm: logFileNm,
+		optsFile: &HandlerOptions{
+			Level:     level,
+			AddTime:   true,
+			AddLevel:  true,
+			AddSource: true,
+		},
+		optsStdout: &HandlerOptions{Level: level},
+		optsStderr: &HandlerOptions{
+			Level:     level,
+			AddLevel:  true,
+			AddSource: true,
+		},
 	}
 
-	optsStderr := &HandlerOptions{
-		Level:    level,
-		AddLevel: true,
+	if logFileNm == "" {
+		l.noFile = true
 	}
-	optsFile := &HandlerOptions{Level: level}
 
-	handlerStdout := NewStdoutHandler(nil)
-	handlerStderr := NewStderrHandler(optsStderr)
-	handlerFile := NewFileHandler(f, optsFile)
+	return l
+}
 
-	handlerMulti := NewMultiHandler(
-		handlerStdout,
-		handlerStderr,
-		handlerFile,
-	)
+func (l *Lager) NoFile(noFile bool) *Lager {
+	l.noFile = noFile
+	return l
+}
+
+func (l *Lager) NoStdout(noStdout bool) *Lager {
+	l.noStdout = noStdout
+	return l
+}
+
+func (l *Lager) NoStderr(noStderr bool) *Lager {
+	l.noStderr = noStderr
+	return l
+}
+
+func (l *Lager) OptsFile(opts *HandlerOptions) *Lager {
+	l.optsFile = opts
+	return l
+}
+
+func (l *Lager) OptsStdout(opts *HandlerOptions) *Lager {
+	l.optsStdout = opts
+	return l
+}
+
+func (l *Lager) OptsStderr(opts *HandlerOptions) *Lager {
+	l.optsStderr = opts
+	return l
+}
+
+func (l *Lager) Init() (*os.File, error) {
+	if l.noFile && l.noStdout && l.noStderr {
+		panic("lager: At least one variable, NoFile, NoStdout, or NoStderr must be false")
+	}
+
+	var f *os.File
+	var err error
+	var handlers []slog.Handler
+
+	if !l.noFile {
+		f, err = os.OpenFile(l.logFileNm, os.O_APPEND|os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			return nil, err
+		}
+
+		handlerFile := NewFileHandler(f, l.optsFile)
+		handlers = append(handlers, handlerFile)
+	}
+
+	if !l.noStdout {
+		handlerStdout := NewStdoutHandler(l.optsStdout)
+		handlers = append(handlers, handlerStdout)
+	}
+
+	if !l.noStderr {
+		handlerStderr := NewStderrHandler(l.optsStderr)
+		handlers = append(handlers, handlerStderr)
+	}
+
+	handlerMulti := NewMultiHandler(handlers...)
 
 	logger := slog.New(handlerMulti)
 	slog.SetDefault(logger)
@@ -35,30 +111,12 @@ func Init(logFileNm string, level slog.Leveler) (*os.File, error) {
 	return f, nil
 }
 
-func Trace(msg string, args ...any) {
-	trace(msg, args...)
-}
-
-func TraceIndent(msg string, indent int, args ...any) {
-	trace(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func DebugIndent(msg string, indent int, args ...any) {
-	slog.Debug(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func WarnIndent(msg string, indent int, args ...any) {
-	slog.Warn(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func ErrorIndent(msg string, indent int, args ...any) {
-	slog.Error(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func InfoIndent(msg string, indent int, args ...any) {
-	slog.Info(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func trace(msg string, args ...any) {
-	slog.Default().Log(context.Background(), LevelTrace, msg, args...)
+func (l *Lager) String() string {
+	s := strings.Builder{}
+	s.WriteString("Lager Settings:\n")
+	s.WriteString(fmt.Sprintf("  Log File Name: %s\n", l.logFileNm))
+	s.WriteString(fmt.Sprintf("  Log to File? %t\n", l.noFile))
+	s.WriteString(fmt.Sprintf("  Log to Stdout? %t\n", l.noStdout))
+	s.WriteString(fmt.Sprintf("  Log to Stderr? %t\n", l.noStderr))
+	return s.String()
 }
