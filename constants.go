27cf143f15363c99ad02e24eea3ed8e92f8e466e
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-12-23 21:15:57 -0500

    Move constants into their own file

---
+++ constants.go
@@ -0,0 +1,11 @@
+package lager
+
+import "log/slog"
+
+const (
+	LevelTrace   = slog.Level(-8)
+	LevelDebug   = slog.LevelDebug
+	LevelInfo    = slog.LevelInfo
+	LevelWarning = slog.LevelWarn
+	LevelError   = slog.LevelError
+)
