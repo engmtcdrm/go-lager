343d286249b0e09a3040dd4da754a2d2ea7e9230
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-12-23 21:16:27 -0500

    Move constants to their own file Added log level to stderr. Reordered funcs based on "log level"

--- lager.go
+++ lager.go
@@ -7,30 +7,21 @@ import (
 	"strings"
 )
 
-const (
-	LevelTrace   = slog.Level(-8)
-	LevelDebug   = slog.LevelDebug
-	LevelInfo    = slog.LevelInfo
-	LevelWarning = slog.LevelWarn
-	LevelError   = slog.LevelError
-)
-
-func trace(msg string, args ...any) {
-	slog.Default().Log(context.Background(), LevelTrace, msg, args...)
-}
-
 func Init(logFileNm string, level slog.Leveler) (*os.File, error) {
 	f, err := os.OpenFile(logFileNm, os.O_APPEND|os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return nil, err
 	}
 
-	opts := &HandlerOptions{Level: level}
-	opts2 := *opts
+	optsStderr := &HandlerOptions{
+		Level:    level,
+		AddLevel: true,
+	}
+	optsFile := &HandlerOptions{Level: level}
 
 	handlerStdout := NewStdoutHandler(nil)
-	handlerStderr := NewStderrHandler(opts)
-	handlerFile := NewFileHandler(f, &opts2)
+	handlerStderr := NewStderrHandler(optsStderr)
+	handlerFile := NewFileHandler(f, optsFile)
 
 	handlerMulti := NewMultiHandler(
 		handlerStdout,
@@ -44,8 +35,20 @@ func Init(logFileNm string, level slog.Leveler) (*os.File, error) {
 	return f, nil
 }
 
+func Trace(msg string, args ...any) {
+	trace(msg, args...)
+}
+
+func TraceIndent(msg string, indent int, args ...any) {
+	trace(strings.Repeat(" ", indent)+msg, args...)
+}
+
 func DebugIndent(msg string, indent int, args ...any) {
 	slog.Debug(strings.Repeat(" ", indent)+msg, args...)
+}
+
+func WarnIndent(msg string, indent int, args ...any) {
+	slog.Warn(strings.Repeat(" ", indent)+msg, args...)
 }
 
 func ErrorIndent(msg string, indent int, args ...any) {
@@ -56,14 +59,6 @@ func InfoIndent(msg string, indent int, args ...any) {
 	slog.Info(strings.Repeat(" ", indent)+msg, args...)
 }
 
-func Trace(msg string, args ...any) {
-	trace(msg, args...)
-}
-
-func TraceIndent(msg string, indent int, args ...any) {
-	trace(strings.Repeat(" ", indent)+msg, args...)
-}
-
-func WarnIndent(msg string, indent int, args ...any) {
-	slog.Warn(strings.Repeat(" ", indent)+msg, args...)
+func trace(msg string, args ...any) {
+	slog.Default().Log(context.Background(), LevelTrace, msg, args...)
 }
