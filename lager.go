01e411087d4d6acb7d74d50d8eaa084870bc574e
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-12-25 09:33:31 -0500

    Added comments for functions and renamed some functions to be easier to understand

--- lager.go
+++ lager.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 )
 
+// Lager represents the logging configuration and setup.
 type Lager struct {
 	noFile     bool
 	noStdout   bool
@@ -20,6 +21,7 @@ type Lager struct {
 	optsStderr *HandlerOptions
 }
 
+// NewLager creates a new Lager instance with the specified log level and log file name.
 func NewLager(level slog.Leveler, logFile string) *Lager {
 	l := &Lager{
 		logFile: logFile,
@@ -38,47 +40,68 @@ func NewLager(level slog.Leveler, logFile string) *Lager {
 	}
 
 	if logFile == "" {
-		l.noFile = true
+		l.NoFile(true)
 	}
 
 	return l
 }
 
+// LogFile sets the log file name.
 func (l *Lager) LogFile(logFile string) *Lager {
 	l.logFile = logFile
 	return l
 }
 
+// NoFile sets whether to disable logging to file.
 func (l *Lager) NoFile(noFile bool) *Lager {
 	l.noFile = noFile
 	return l
 }
 
+// NoStdout sets whether to disable logging to stdout.
 func (l *Lager) NoStdout(noStdout bool) *Lager {
 	l.noStdout = noStdout
 	return l
 }
 
+// NoStderr sets whether to disable logging to stderr.
 func (l *Lager) NoStderr(noStderr bool) *Lager {
 	l.noStderr = noStderr
 	return l
 }
 
-func (l *Lager) OptsFile(opts *HandlerOptions) *Lager {
+// FileOptions sets the handler options for file logging.
+func (l *Lager) FileOptions(opts *HandlerOptions) *Lager {
+	if opts == nil {
+		opts = &HandlerOptions{}
+	}
+
 	l.optsFile = opts
 	return l
 }
 
-func (l *Lager) OptsStdout(opts *HandlerOptions) *Lager {
+// StdoutOptions sets the handler options for stdout logging.
+func (l *Lager) StdoutOptions(opts *HandlerOptions) *Lager {
+	if opts == nil {
+		opts = &HandlerOptions{}
+	}
+
 	l.optsStdout = opts
 	return l
 }
 
-func (l *Lager) OptsStderr(opts *HandlerOptions) *Lager {
+// StderrOptions sets the handler options for stderr logging.
+func (l *Lager) StderrOptions(opts *HandlerOptions) *Lager {
+	if opts == nil {
+		opts = &HandlerOptions{}
+	}
+
 	l.optsStderr = opts
 	return l
 }
 
+// Init initializes the logging system based on the configured settings.
+// It returns the file handle if logging to a file is enabled, otherwise nil.
 func (l *Lager) Init() (*os.File, error) {
 	if l.noFile && l.noStdout && l.noStderr {
 		panic("lager: At least one variable, NoFile, NoStdout, or NoStderr must be false")
@@ -109,19 +132,19 @@ func (l *Lager) Init() (*os.File, error) {
 	}
 
 	handlerMulti := NewMultiHandler(handlers...)
-
 	logger := slog.New(handlerMulti)
 	slog.SetDefault(logger)
 
 	return f, nil
 }
 
+// String returns a string representation of the Lager configuration.
 func (l *Lager) String() string {
 	s := strings.Builder{}
 	s.WriteString("Lager Settings:\n")
-	s.WriteString(fmt.Sprintf("  Log File Name:    %s\n", l.logFile))
-	s.WriteString(fmt.Sprintf("  Log to File?      %t\n", !l.noFile))
-	s.WriteString(fmt.Sprintf("  Log to Stdout?    %t\n", !l.noStdout))
-	s.WriteString(fmt.Sprintf("  Log to Stderr?    %t\n", !l.noStderr))
+	s.WriteString(fmt.Sprintf("  Log File Name   : %s\n", l.logFile))
+	s.WriteString(fmt.Sprintf("  Log to File     ? %t\n", !l.noFile))
+	s.WriteString(fmt.Sprintf("  Log to Stdout   ? %t\n", !l.noStdout))
+	s.WriteString(fmt.Sprintf("  Log to Stderr   ? %t\n", !l.noStderr))
 	return s.String()
 }
